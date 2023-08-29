d3ca77f4afde7985a5d6dd007af9e8d89cbc35e2
karim1311 <valenzuelakarim03@gmail.com>
2023-08-29 13:13:07 -0700

    icono location

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,63 +1,73 @@
-{/* archivo de Search. js dentro de carpeta Search dentro de carpeta components */}
-import React from 'react'
-import { useState } from 'react';
-import "../Search/Search.css"
+{
+  /* archivo de Search. js dentro de carpeta Search dentro de carpeta components */
+}
+import React from "react";
+import { useState } from "react";
+import "../Search/Search.css";
 
-export default function Search( { onCityChange } ) {
-    const [city, setCity] = useState("");
+export default function Search({ onCityChange }) {
+  const [city, setCity] = useState("");
 
-    const handleSearch = () => {
-        onCityChange(city);
-      };
-    
+  const handleSearch = () => {
+    onCityChange(city);
+  };
+
   return (
-    <div className='d-flex w-100 justify-content-between p-4'>
+    <div className="d-flex w-100 justify-content-between p-4">
+      <button
+        className="btn btn-secondary d-flex flex-start"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasWithBothOptions"
+        aria-controls="offcanvasWithBothOptions"
+      >
+        Search for places
+      </button>
+
+      <button className="btn btn-secondary">
+        <span class="material-symbols-outlined">my_location</span>
+      </button>
+
+      <div
+        className="offcanvas offcanvas-start fondo"
+        data-bs-scroll="true"
+        tabindex="-1"
+        id="offcanvasWithBothOptions"
+        aria-labelledby="offcanvasWithBothOptionsLabel"
+      >
+        <div className="offcanvas-header fondo">
+          <h5
+            className="offcanvas-title"
+            id="offcanvasWithBothOptionsLabel"
+          ></h5>
           <button
-          className="btn btn-secondary d-flex flex-start"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasWithBothOptions"
-          aria-controls="offcanvasWithBothOptions"
-          >
-         Search for places
-          </button>
-
-          <button className='btn btn-secondary'>Hello</button>
-
-          <div
-          className="offcanvas offcanvas-start fondo"
-          data-bs-scroll="true"
-          tabindex="-1"
-          id="offcanvasWithBothOptions"
-          aria-labelledby="offcanvasWithBothOptionsLabel"
-          >
-          <div className="offcanvas-header fondo">
-            <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body fondo">
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body fondo">
           <div className="search-container">
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleSearch();
-            }
-          }}
-        />
-        <button className='border-0 text-light btn-search' onClick={handleSearch}>Search</button>
-      </div>
-
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                }
+              }}
+            />
+            <button
+              className="border-0 text-light btn-search"
+              onClick={handleSearch}
+            >
+              Search
+            </button>
           </div>
         </div>
-        </div>
-  )
+      </div>
+    </div>
+  );
 }

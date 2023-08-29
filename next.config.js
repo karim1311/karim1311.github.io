c81a26818d7c2c669302ada755860d7f971b0a7f
karim1311 <valenzuelakarim03@gmail.com>
2023-08-29 11:44:14 -0700

    git pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export',
+    basePath: '/gh-pages-test',
+  }
+  
+  module.exports = nextConfig
